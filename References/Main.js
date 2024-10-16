a18086fd0aedf5e86a83f76ba86e1fed20b9e356
KaliBasenji42 <jacobdelzer369@gmail.com>
2024-10-16 14:02:05 -0600

    Update Main.js format

--- References/Main.js
+++ References/Main.js
@@ -52,7 +52,7 @@ for(let i = 0; i < body.length; i++) body[i].innerHTML += bodyHTML;
 
 document.addEventListener('DOMContentLoaded', function (event) {
   document.querySelector('body').style = "animation-name: load;" +
-    "animation-duration: 0.75s;";
+                                         "animation-duration: 0.75s;";
     loadSpin();
 });
 
