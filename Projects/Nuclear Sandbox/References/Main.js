4bfa633332cb380e84e8aa2e01421e610d122503
Jacob D - Home <jacobdelzer369@gmail.com>
2024-11-18 00:56:24 -0700

    Decay Tree (tempElems iteration error)

--- Projects/Nuclear Sandbox/References/Main.js
+++ Projects/Nuclear Sandbox/References/Main.js
@@ -7,10 +7,14 @@ const bodyHTML = `
   `;
 
 let menus;
-
 let currentMenu = 'none';
 
 let fileNameInput;
+
+let nuDatStatOutput;
+let NuDat = {};
+let DTForm;
+let decayTree = {};
 
 // Functions
 
@@ -52,6 +56,39 @@ function openFile() {
   
 }
 
+function loadData() {
+  
+  fetch('References/Data/NuDat.json')
+    .then(response => {
+      
+      if(!response.ok) {
+        
+        nuDatStatOutput.innerHTML = '🛜⚠️';
+        
+        throw new Error('Response: ' + response.statusText);
+        
+      }
+      
+      return response.json();
+      
+    })
+    .then(data => {
+      
+      NuDat = data;
+      
+      nuDatStatOutput.innerHTML = '✅';
+      
+    })
+    .catch(error => {
+      
+      nuDatStatOutput.innerHTML = '⚠️';
+      
+      console.log(error);
+      
+    });
+  
+}
+
 // Events
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -61,6 +98,10 @@ document.addEventListener('DOMContentLoaded', function() {
   menus = document.getElementsByClassName('menu');
   
   fileNameInput = document.getElementById('FileName');
+  
+  nuDatStatOutput = document.getElementById('NuDatStat');
+  
+  DTForm = document.getElementById('DTForm');
   
   // Menu
   
@@ -80,6 +121,49 @@ document.addEventListener('DOMContentLoaded', function() {
     menuXImg.title = '[ESC]';
     
   }
+  
+  // Data
+  
+  loadData();
+  
+  // DT
+  
+  DTForm.addEventListener("submit", function(event) {
+    
+    event.preventDefault();
+    
+    let elemStr = document.getElementById('DTElemInput').value;
+    let parent = NuDat[elemStr]['levels'][0];
+    
+    let field = document.getElementById('DTField');
+    
+    let elems = new Set();
+    let newElems = new Set();
+    
+    newElems.add(parent);
+    
+    while(newElems.size > 0) {
+      
+      let tempElems = new Set(newElems);
+      
+      console.log(tempElems);
+      
+      elems = elems.union(newElems);
+      newElems.clear();
+      
+      for(let i = 0; i < tempElems.size; i++) {
+        
+        let modes = tempElems[i];
+        
+        console.log(modes);
+        
+      }
+      
+    }
+    
+    console.log(elems);
+    
+  });
   
 });
 
