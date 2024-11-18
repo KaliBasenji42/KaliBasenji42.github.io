3d19afab580f73d64c0137eaa9f7a4e58a05ec02
Jacob D - Home <jacobdelzer369@gmail.com>
2024-11-17 20:05:53 -0700

    File Menu Base

--- Projects/Nuclear Sandbox/References/Main.js
+++ Projects/Nuclear Sandbox/References/Main.js
@@ -6,6 +6,87 @@ const headHTML = `
 const bodyHTML = `
   `;
 
+let menus;
+
+let currentMenu = 'none';
+
+let fileNameInput;
+
 // Functions
 
+function closeMenus() {
+  
+  for(let i = 0; i < menus.length; i++) {
+    
+    menus[i].style.display = 'none';
+    
+  }
+  
+  currentMenu = 'none';
+  
+}
+
+function openMenu(ID) {
+  
+  closeMenus();
+  
+  document.getElementById(ID).style.display = 'block';
+  
+  currentMenu = ID;
+  
+}
+
+function saveFile() {
+  
+  input = fileNameInput.value;
+  
+  console.log('Save: ' + input);
+  
+}
+
+function openFile() {
+  
+  input = fileNameInput.value;
+  
+  console.log('Open: ' + input);
+  
+}
+
 // Events
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // Elems
+  
+  menus = document.getElementsByClassName('menu');
+  
+  fileNameInput = document.getElementById('FileName');
+  
+  // Menu
+  
+  for(let i = 0; i < menus.length; i++) {
+    
+    // X
+    
+    let menuX = document.createElement('button');
+    menus[i].appendChild(menuX);
+    menuX.className = 'bttn menuX';
+    menuX.onclick = function() {closeMenus();}
+    
+    let menuXImg = document.createElement('img');
+    menuX.appendChild(menuXImg);
+    menuXImg.className = 'bttnImg';
+    menuXImg.src = 'References/Images/X.png';
+    menuXImg.title = '[ESC]';
+    
+  }
+  
+});
+
+document.addEventListener('keyup', function() {
+  
+  if(event.key == 'Escape') closeMenus();
+  
+  if(event.key == 'f' || event.key == 'F') openMenu('MenuSave');
+  
+});
