bfc2ca26dae79ca97bc749b3a0a1c9c6c341c8d8
KaliBasenji42 <jacobdelzer369@gmail.com>
2024-12-23 07:34:06 +0000

    Short Stories

--- Projects/Short Stories/References/Main.js
+++ Projects/Short Stories/References/Main.js
@@ -8,7 +8,6 @@ const bodyHTML = `
   <div class="navigation">
     
     <a class="nav" title="Q/q" href="index.html">Home</a>
-    <a class="nav" title="W/w" href="TOC.html">Table of Contents</a>
     <a class="nav" title="E/e" href="Ideas.html">Ideas</a>
     
   </div>
@@ -25,6 +24,5 @@ for(let i = 0; i < chapters.length; i++) chapterNames.push('Story ' + (i + 1) + 
 
 document.addEventListener('keypress', function() {
   shortcut("q", "Q", "index.html");
-  shortcut("w", "W", "TOC.html");
   shortcut("e", "E", "Ideas.html");
 });
