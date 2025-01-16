1d00ae86c3f1a92921966a6a538f01264ddc6a4d
KaliBasenji42 <jacobdelzer369@gmail.com>
2025-01-16 20:49:16 +0000

    Pending changes exported from your codespace

--- Projects/Misc/References/Main.js
+++ Projects/Misc/References/Main.js
@@ -8,17 +8,16 @@ const bodyHTML = `
   <div class="navigation">
     
     <a class="nav" title="Q/q" href="index.html">Home</a>
-    <a class="nav" title="E/e" href="Ideas.html">Ideas</a>
     
   </div>
   
   `;
 
-const chapters = ['1.html'];
+const chapters = ['Start.html', 'Eureka.html'];
 
-const chapterNames = ['&#9608&#9608&#9608&#9608'];
+const chapterNames = ['Fire', 'Publication ██42'];
 
-for(let i = 0; i < chapterNames.length; i++) chapterNames[i] = chapterNames[i] + ' | ' + 'Short Story ' + (i + 1);
+for(let i = 0; i < chapterNames.length; i++) chapterNames[i] = (i + 1) + ' | ' + chapterNames[i];
 
 // Events
 
