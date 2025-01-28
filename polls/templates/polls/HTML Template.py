abea9ac690cda8169864f4e4e1f7a44aae886c91
Jacob <jacobdelzer369@gmail.com>
2025-01-27 19:55:30 -0700

    Fixed HTML Links

--- polls/templates/polls/HTML Template.py
+++ polls/templates/polls/HTML Template.py
@@ -15,6 +15,18 @@ end = '<!--End-->\n'
 
 projectId = '<Project>'
 
+# Functions
+
+def cutStringAt(string, char):
+  
+  for i in range(len(string)):
+    
+    if string[i] == char: return string[:i]
+    
+  
+  return string
+  
+
 # Getting Paths
 
 print('\nGetting file paths:\n')
@@ -97,49 +109,66 @@ print('\nWriting to Files:\n')
 
 for path in paths:
   
-  cont = []
+  cont = [] # Content of file before writing
   
   try:
     
     with open(path, 'r+', encoding='utf-8', errors='ignore') as file: cont = file.readlines()
     
   except: print('Unable to open "' + path + '" :/')
+  
   else:
     
-    addCont = True # If it should add content, not template
+    addCont = True # If it should add content, not template section
     
-    newCont = []
+    newCont = [] # Content to be writen to file
     
-    sect = 0
+    sect = 0 # Template section
+    
+    project = cutStringAt(path, '\\')
+    
+    # Start/End
     
     for line in cont:
       
-      # Find ProjectId
-      
-      # Start/End
-      
-      if line == end:
+      if line == end: # End of template section
         
         newCont.append(line)
         
         addCont = True
         
       
-      elif line == start:
+      elif line == start: # Start of template section
         
         newCont.append(line)
         
         addCont = False
         
-        newCont = newCont + template[sect]
+        newCont = newCont + template[sect] # Add template content
         
-        sect += 1
+        sect += 1 # Iterate
+        
+        # Ignore old HTML file content
         
       
       elif addCont: newCont.append(line)
+      # ^^^ Add old HTML file content
       
     
+    # Replace Project Id
+    
+    for i in range(len(newCont)):
+      
+      line = newCont[i].replace(projectId, project)
+      newCont[i] = line
+      
+      
+    
+    # Write to file
+    
     with open(path, 'w', encoding='utf-8', errors='ignore') as file: file.writelines(newCont)
+    
+    # Print
     
     print('Updated "' + path + '"')
     
