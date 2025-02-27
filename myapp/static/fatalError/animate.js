7d4a25aaa6dd0aa8c2b57a5a481203cbadf71b83
Jacob <jacobdelzer369@gmail.com>
2025-02-27 00:23:44 -0700

    Image Update

--- myapp/static/fatalError/animate.js
+++ myapp/static/fatalError/animate.js
@@ -4,9 +4,9 @@ function Umbriel() {
   
   const bttn = document.getElementById("Umbriel");
   
-  if(i % 2 == 0) bttn.innerHTML = '<img src = "../../../../staticfiles/Fatal Error - System Restarting/Umbriel1.png" alt = "Umbriel" width = "128" height = "128">';
+  if(i % 2 == 0) bttn.innerHTML = '<img src = "../../../static/fatalError/Umbriel1.png" alt = "Umbriel" width = "128" height = "128">';
     
-  if(i % 2 == 1) bttn.innerHTML = '<img src = "../../../../staticfiles/Fatal Error - System Restarting/Umbriel2.png" alt = "Umbriel" width = "128" height = "128">';
+  if(i % 2 == 1) bttn.innerHTML = '<img src = "../../../static/fatalError/Umbriel2.png" alt = "Umbriel" width = "128" height = "128">';
   
   i ++;
   
