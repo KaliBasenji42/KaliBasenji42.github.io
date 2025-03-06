54172734733183861a813a25fbc5227b64df63c7
KaliBasenji42 <jacobdelzer369@gmail.com>
2025-03-06 14:34:01 -0700

    Post setup.py: 'temp' & minor link errors

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ run = True
 instructions = [
   'Input "quit" to quit',
   'Input "?" to reprint this',
-  'Input "temp" to update template marked areas',
-  'Input "django" to set up links to for Djando',
-  '  WARNING: "django" unfinished and will not work!'
+  'Input "temp" to update template marked areas'
 ]
 
 paths = []
@@ -17,12 +15,7 @@ exts = [
   '.html'
 ]
 
-jsPaths = []
-jsExts = [
-  '.js'
-]
-
-location = '.'
+location = 'projects/'
 
 # Template
 
@@ -34,19 +27,6 @@ tempEnd = '<!--End-->\n'
 
 projectId = '{project}'
 projectDepth = len('projects/') # What to cut off in front of Project
-
-# Django
-
-urlsPath = 'urls.py'
-viewsPath = 'views.py'
-
-urls = []
-
-allowedAttributes = ['href', 'src']
-allowedElements = ['a', 'img', 'link', 'script']
-
-djangoInject = '# inject\n'
-djangoInjectEnd = '# inject end\n'
 
 # Simple Functions
 
@@ -103,27 +83,6 @@ def getPaths():
             
           paths.append(path)
           print('Added "' + path + '" to paths')
-          
-        
-      
-      # JS
-      
-      jsMatch = False
-      
-      for ext in jsExts:
-        if path[-len(ext):] == ext: jsMatch = True
-      
-      if jsMatch:
-        
-        try: 
-          
-          with open(path, 'r+', encoding='utf-8', errors='ignore') as file: pass
-          
-        except: print('Unable to open "' + path + '" :/')
-        else:
-            
-          jsPaths.append(path)
-          print('Added "' + path + '" to jsPaths')
           
         
       
@@ -252,157 +211,6 @@ def templateEdit():
     
   
 
-def djangoUrls():
-  
-  print('Getting URLs:\n')
-  
-  for path in paths:
-    
-    cont = [] # Content of file before writing
-    
-    try:
-      
-      with open(path, 'r+', encoding='utf-8', errors='ignore') as file: cont = file.read()
-      
-    except:
-      
-      print('Unable to open "' + path + '" :/')
-      
-      return False # Fail
-    
-    else:
-      
-      isJs = False
-      
-      for ext in jsExts:
-        if path[-len(ext):] == ext: isJs = True
-      
-      # js comment thing
-      
-      inElement = False # If in an allowedElements
-      afterAttribute = False # If after an allowedAttributes
-      inString = False # If in quotes
-      
-      url = ''
-      
-      for i in range(len(cont)): # Iterate characters in file
-        
-        # JS
-        
-        if isJs:
-          
-          pass
-          # WIP
-          
-        
-        # In Element
-        
-        if not inElement:
-          for element in allowedElements:
-            if i > len('<'+element):
-              if cont[i-len('<'+element):i] == '<' + element: inElement = True
-        
-        # After Attribute
-        
-        if not afterAttribute and inElement:
-          for attribute in allowedAttributes:
-            if i > len(attribute):
-              if cont[i-len(attribute):i] == attribute: afterAttribute = True
-        
-        # In String
-        
-        if inString and cont[i] == '"': # End
-          
-          inElement = False
-          afterAttribute = False
-          inString = False
-          
-          # Normalize & Add URL
-          
-          if len(url) >= len('https://'):
-            if url[:len('https://')] == 'https://': break
-          
-          pathPos = remFile(path)
-          
-          normPath = os.path.normpath(os.path.join(pathPos, url))
-          
-          if normPath[:len('myapp')] == 'myapp': normPath = normPath[len('myapp/'):]
-          
-          if normPath.find('{project}') < 0:
-            
-            urls.append(normPath)
-            
-            print('Added "' + normPath + '"')
-            
-          
-          url = ''
-          
-        
-        elif afterAttribute and cont[i] == '"': inString = True # Start
-        
-        elif inString: url = url + cont[i] # In String: Add to URL
-        
-      
-    
-  
-  return True # Success
-  
-
-def djangoSetFiles():
-  
-  if input('Enter "y" to continue: ') != 'y': return
-  
-  print('\nWriting to Files:\n')
-  
-  # urls
-  
-  urlsCont = [] # Content of file before writing
-  
-  try:
-    
-    with open(urlsPath, 'r+', encoding='utf-8', errors='ignore') as file: urlsCont = file.readlines()
-    
-  except: print('Unable to open "' + urlsPath + '" :/')
-  
-  else:
-    
-    newCont = [] # Content to be writen to file
-    
-    inject = False # Should inject
-    
-    for line in urlsCont:
-      
-      if line == djangoInject: 
-        
-        inject = True
-        newCont.append(line)
-        
-        for i in range(len(urls)):
-          
-          newCont.append(
-            '  path("' + urls[i] + '", views.injected' + str(i) + '),\n'
-          )
-          
-        
-      elif line == djangoInjectEnd: inject = False
-      
-      if not inject:
-        
-        newCont.append(line)
-        
-      
-    
-    # Write to file
-    
-    with open(urlsPath, 'w', encoding='utf-8', errors='ignore') as file: file.writelines(newCont)
-    
-    # Print
-    
-    print('Updated "' + urlsPath + '"')
-    
-  
-
-
 # Pre Loop
 
 getPaths()
@@ -429,13 +237,5 @@ while run:
     if readTemplate():
       print()
       templateEdit()
-    
-  
-  elif inp == 'django':
-    
-    if djangoUrls():
-      print()
-      djangoSetFiles()
-      urls = []
     
   
