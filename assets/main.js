e0e42e1b5c7b82f9273952db1baff1869fd56da9
Jacob Delzer <jacobdelzer369@gmail.com>
2025-07-03 14:03:13 -0600

    Theme

--- assets/main.js
+++ assets/main.js
@@ -66,6 +66,110 @@ document.addEventListener('DOMContentLoaded', function () {
   loadSpin();
 });
 
+// Theme
+
+let sheet = new CSSStyleSheet();
+document.adoptedStyleSheets.push(sheet);
+
+let styleSheetStrings = {
+  'default': `
+body {
+  color: rgb(0, 0, 0);
+  background-color: rgb(224, 160, 244);
+  background-image:
+    linear-gradient(135deg, rgb(224, 192, 255), rgb(224, 128, 244)),
+    linear-gradient(90deg, rgb(224, 192, 255), rgb(224, 128, 244));
+}
+
+table {
+  color: rgb(0, 0, 0);
+  background-color: rgb(224, 224, 224);
+}
+  `,
+  'dark': `
+body {
+  color: rgb(255, 255, 255);
+  background-color: rgb(0, 0, 0);
+  background-image: none;
+  background-image: 
+    linear-gradient(135deg, rgb(0, 0, 0) 60%, rgb(0, 0, 16), rgb(16, 0, 16)),
+    linear-gradient(90deg, rgb(0, 0, 0) 60%, rgb(0, 0, 16), rgb(16, 0, 16));
+}
+
+table {
+  color: rgb(225, 225, 225);
+  background-color: rgb(32, 32, 32) !important;
+}
+  `,
+  'light': `
+body {
+  color: rgb(0, 0, 0);
+  background-color: rgb(224, 224, 224);
+  background-image: 
+    linear-gradient(135deg, rgb(224, 224, 224) 60%, rgb(240, 240, 224), rgb(240, 224, 224)),
+    linear-gradient(90deg, rgb(224, 224, 224) 60%, rgb(240, 240, 224), rgb(240, 224, 224));
+}
+
+table {
+  color: rgb(0, 0, 0);
+  background-color: rgb(224, 224, 224);
+}
+  `
+};
+
+function updateTheme() {
+  
+  let theme = localStorage.getItem('theme');
+  
+  sheet.replace(
+    styleSheetStrings[theme]
+  );
+  
+  
+  
+}
+
+function nextTheme(add) {
+  
+  // Variables
+  
+  let themeKeys = Object.keys(styleSheetStrings);
+  
+  let currentTheme = localStorage.getItem('theme');
+  let currentPos = themeKeys.indexOf(currentTheme);
+  if(currentPos == -1) currentPos = 0;
+  
+  let themeButton = document.getElementById('themeButton');
+  
+  // Update Theme
+  
+  currentPos = (currentPos + add) % themeKeys.length;
+  
+  localStorage.setItem('theme', themeKeys[currentPos]);
+  
+  updateTheme();
+  
+  // Update themeButton Title
+  
+  let titleStr = 'Change Theme:\n';
+  
+  for(let i = 0; i < themeKeys.length; i++) {
+    
+    if(i == currentPos) titleStr += '✅ ';
+    else titleStr += '⚫ ';
+    
+    titleStr += themeKeys[i] + '\n';
+    
+  }
+  
+  themeButton.title = titleStr;
+  
+}
+
+updateTheme();
+
+setTimeout(nextTheme, 10, 0);
+
 // :P
 
 document.addEventListener('keypress', function() {
