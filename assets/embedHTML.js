63beeef6ae0318fabf1a2a2cf34a6b9be8a1c330
Jacob D - Laptop <jacobdelzer369@gmail.com>
2025-10-21 16:31:03 -0600

    EmbedHTML.js - Allow for querySelector str

--- assets/embedHTML.js
+++ assets/embedHTML.js
@@ -1,18 +1,18 @@
 // Function
 
-async function embedCont(embedPath) {
+async function embedCont(embedPath, elemSel = '#embedContainer') {
   
   let file = await fetch(embedPath); // Fetch raw file
   //console.log(file);
   
   if(!file.ok) { // Error fetching file
-    document.getElementById('embedContainer').innerHTML = '⚠️ Error Fetching File';
+    document.querySelector(elemSel).innerHTML = '⚠️ Error Fetching File';
     return;
   }
   
   let text = await file.text(); // Raw --> Text
   //console.log(text);
   
-  document.getElementById('embedContainer').innerHTML = text;
+  document.querySelector(elemSel).innerHTML = text;
   
 };
