51af5e29ccd4152d39c090a3fca1ab3666a6c23a
Jacob D - Laptop <code.strike366@passinbox.com>
2026-03-06 11:19:36 -0700

    Update Crazy?!

--- projects/c/assets/main.js
+++ projects/c/assets/main.js
@@ -1,3 +1,43 @@
+// Variables
+
+texts = [ // Different texts
+`Crazy?!
+I was crazy once.
+They locked me in a room.
+A round room.
+A round rubber room.
+A round rubber room with rats.
+Round rats.
+Round rubber rats.
+Round rubber rats with wheels.
+Round wheels.
+Round rubber wheels.
+Round rubber wheels that went *eek eek eek*.
+It drove me crazy.`,
+`Crazy?!
+I was crazy once
+They locked me in a *round* room
+And they only fed me bagles
+Bagles are round
+The sun is round
+The sun is yellow
+Bannas are yellow
+Bannas have spots
+Old people have spots
+Old people live *long* lives
+Life?
+Thats my favorite cereal
+I once bought a box of cereal for $10
+$10?
+Thats crazy!`
+]
+
+textIndex = 0; // Current text
+textBuffer = 4; // Buffer lines
+
+let crazySheet = new CSSStyleSheet(); // Style sheet for text
+document.adoptedStyleSheets.push(crazySheet);
+
 // Functions
 
 function randomMovement(elem, margin) {
@@ -9,6 +49,41 @@ function randomMovement(elem, margin) {
   elem.style.transform = 'scale(' + (Math.random() + 0.5) + ',' + (Math.random() + 0.5) + ')';
   
 } 
+
+function iterateText() {
+  
+  // Iterate
+  
+  textIndex = (textIndex + 1) % texts.length;
+  
+  // Variables
+  
+  textElem = document.getElementById('crazyTxt'); // Element
+  
+  textArr = texts[textIndex].split('\n'); // Text as array
+  textLen = textArr.length; // Length of text
+  styleLen = (textLen * 1.5); // Number for style movement
+  
+  // Set elem text
+  
+  text = texts[textIndex]; // OG text
+  
+  for(let i = 0; i < textBuffer; i++) { // Add buffer
+    text += '\n' + textArr[i];
+  }
+  
+  textElem.innerText = text; // Set
+  
+  // Style
+  
+  crazySheet.replace(
+    '@keyframes crazy {' +
+    '  from {top: 0}' +
+    '  to {top: -' + styleLen + 'em}' +
+    '}'
+  );
+  
+}
 
 // Event Listener
 
