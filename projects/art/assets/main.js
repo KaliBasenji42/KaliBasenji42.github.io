a267a192410cea6a2094fb0379873d4a6d129a51
Jacob D - Laptop <code.strike366@passinbox.com>
2026-04-23 23:47:16 -0600

    Art re-structure, re-design, and cactus addition

--- projects/art/assets/main.js
+++ projects/art/assets/main.js
@@ -34,6 +34,8 @@ function zoom(URL) { // Zoom on image (onclick)
 
 document.addEventListener('DOMContentLoaded', function() {
   
+  // Zoom
+  
   zoomElem = document.getElementById('zoom'); // Get zoom element
   zoomImg = document.querySelector('#zoom > img'); // Get zoom image
   zoomLink = document.querySelector('#zoom > a'); // Get zoom image
@@ -42,6 +44,22 @@ document.addEventListener('DOMContentLoaded', function() {
   
   for(const image of images) { // For each image
     image.onclick = () => {zoom(image.src)}; // Onclick
+  }
+  
+  // Cont
+  
+  let contLinks = document.querySelectorAll('.cont > .imgCont');
+  // Link image/buttons in cont
+  
+  for(const link of contLinks) { // For each link
+    link.onmouseover = () => { // Function on mouse over
+      let rand = (Math.random() * 8) + 2; // Random num between 2 and 10
+      rand = rand * ((Math.floor(Math.random() * 2) * 2) - 1); // Random +/-
+      link.style.transform = "rotate(" + rand + "deg) scale(1.05)";
+    };
+    link.onmouseout = () => { // Function on mouse out
+      link.style.transform = "rotate(0deg) scale(1)";
+    }
   }
   
 });
